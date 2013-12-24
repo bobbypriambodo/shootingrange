b695475494123277817822b424d5916ddc2c7c83
Widyanto Bagus Priambodo <bobby.priambodo@gmail.com>
2013-12-24 22:03:57 +0700

    using macros, player can move

--- Program.asm
+++ Program.asm
@@ -15,6 +15,36 @@
 .def A  = r19
 .def position = r23
 
+.equ pos1 = 0x80
+.equ pos2 = 0xC0
+.equ pos3 = 0x94
+.equ pos4 = 0xD4
+
+.equ player = 125
+.equ target = 226
+.equ space = 32
+
+.macro set_cursor ;(@0: to)
+	cbi PORTA,1	; CLR RS
+	ldi PB,@0
+	out PORTB,PB
+	sbi PORTA,0	; SETB EN
+	cbi PORTA,0	; CLR EN
+	rcall lcd_wait
+.endmacro
+
+.macro move ;(@0: from, @1: to)
+	draw @0,space
+	draw @1,player
+	ldi position,@1
+.endmacro
+
+.macro draw ;(@0: at, @1: asciicode)
+	set_cursor @0
+	ldi A,@1
+	rcall write_text
+.endmacro
+
 ;############### INITIALIZATION ################
 start:
 	ldi temp,low(RAMEND)
@@ -42,113 +72,40 @@ int_enable:
 	
 ;############### SPLASH SCREEN ################
 show_welcome_message:
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xC3
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
+	set_cursor 0xC3
 	ldi	ZH,high(2*welcome_message)	; Load high part of byte address into ZH
 	ldi	ZL,low(2*welcome_message)	; Load low part of byte address into ZL
 	rcall loadbyte
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0x9C
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
+	set_cursor 0x9C
 	ldi	ZH,high(2*version)	; Load high part of byte address into ZH
 	ldi	ZL,low(2*version)	; Load low part of byte address into ZL
 	rcall loadbyte
 	
-	ldi temp, 70
+	ldi temp,20;70
 	rcall delay
 	rcall lcd_clear
 
-
 init_elements:
 	;player
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xC0
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 125
-	rcall write_text
+	draw pos3,player
+	ldi position,pos3
 	;target 1
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0x8E
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
+	draw 0x8E,target
 	;target 2
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xD3
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
+	draw 0xD3,target
 	;target 3
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xA0
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
+	draw 0xA0,target
 	;target 4
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xE5
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
+	draw 0xE5,target
 	;target 5
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0x92
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
+	draw 0x92,target
 	;target 6
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xCF
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
+	draw 0xCF,target
 	;target 7
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xA4
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
+	draw 0xA4,target
 	;target 8
-	cbi PORTA,1	; CLR RS
-	ldi PB, 0xE1
-	out PORTB,PB
-	sbi PORTA,0	; SETB EN
-	cbi PORTA,0	; CLR EN
-	rcall lcd_wait
-	ldi A, 226
-	rcall write_text
-
+	draw 0xE1,target
+	
 ;############### GAME LOOP ################
 loop:
 	rjmp loop
@@ -158,25 +115,29 @@ ext_int0:	;up button
 	push r16
 	in r16,sreg
 	push r16
-
-	rcall lcd_clear
-
+	cpi position,pos1
+	breq exit0
+	rcall move_up
+exit0:
 	pop r16
 	out sreg,r16
 	pop r16
 	reti
 
 ext_int1:	;down button
-	reti
-
-ext_int2:	;shoot button
 	push r16
 	in r16,sreg
 	push r16
-
+	cpi position,pos4
+	breq exit1
+	rcall move_down
+exit1:
 	pop r16
 	out sreg,r16
 	pop r16
+	reti
+
+ext_int2:	;shoot button
 	reti
 	
 ;############### SUBROUTINES ################
@@ -252,7 +213,39 @@ write_text:
 	cbi PORTA,0	; CLR EN
 	rcall lcd_wait
 	ret
-	
+
+move_up:
+	cpi position,pos2
+	breq up_to_pos1
+	cpi position,pos3
+	breq up_to_pos2
+	rjmp up_to_pos3
+up_to_pos1:
+	move pos2,pos1
+	ret
+up_to_pos2:
+	move pos3,pos2
+	ret
+up_to_pos3:
+	move pos4,pos3
+	ret
+
+move_down:
+	cpi position,pos1
+	breq down_to_pos2
+	cpi position,pos2
+	breq down_to_pos3
+	rjmp down_to_pos4
+down_to_pos2:
+	move pos1,pos2
+	ret
+down_to_pos3:
+	move pos2,pos3
+	ret
+down_to_pos4:
+	move pos3,pos4
+	ret
+
 ;############### STRINGS ################
 welcome_message:
 .db "Shooting Range"
